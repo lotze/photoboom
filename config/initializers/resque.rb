3aeb665e78e397173f3e057b782ac5d5ec7c70f5
Thomas Lotze <thomaslotze@thomaslotze.com>
2018-01-31 00:34:48 -0500

    avoid trying to initialize redis if unavailable

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,5 @@
-Resque.logger.level = Logger::DEBUG
-
-Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
-Resque.schedule = YAML.load_file(File.join(File.dirname(__FILE__), '../resque_schedule.yml'))
+if ENV.include?('REDIS_URL')
+  Resque.logger.level = Logger::DEBUG
+  Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+  Resque.schedule = YAML.load_file(File.join(File.dirname(__FILE__), '../resque_schedule.yml'))
+end
