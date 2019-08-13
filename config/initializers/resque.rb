168c6f7bcf0e56d90c889c42f7420a6b20dc66fe
Thomas Lotze <thomas@thomaslotze.com>
2019-08-12 20:05:20 -0700

    Note about resque on heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,5 @@
+# Note: may need to set RAILS_RESQUE_REDIS as well as REDIS_URL
+# https://stackoverflow.com/questions/42388458/redis-tries-to-connect-to-localhost-on-heroku-instead-of-redis-url
 if ENV.include?('REDIS_URL')
   Resque.logger.level = Logger::DEBUG
   Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
